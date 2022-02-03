fd3e72ad770f579cb95357b2f3f43c126ea67bca
jago-gif <halcon1411@gmail.com>
2022-02-03 18:36:02 -0300

    dafd

--- desafio/fichas.js
+++ desafio/fichas.js
@@ -1,46 +1,43 @@
-
 //variable contenedor
 const contenedor = document.querySelector('tbody')
 let resultados = ''
 
-//fichas.js es el script para ver, editar, crear y eliminar fichas de registro de pacientes
-        const baseURL   = "https://hwwyuypdcxdigyfznqcw.supabase.co";
-        const apiCall   = '/rest/v1/f-registro'
-        const apiKey    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjQzODM4OTEyLCJleHAiOjE5NTk0MTQ5MTJ9.tNykPvbzGeUsS7UoP2mNJNBjKUG6R76HPcNtLT1TjBc'
-        const url       = baseURL + apiCall
 
-        
-        //funcion para mostrar los resultados
-        const mostrar = (pacientes) => {
-            pacientes.array.for(paciente => {
-                resultados +=   
-                                <tr>
-                                <td>${paciente.pname}</td>
-                                <td>${paciente.tutor}</td>
-                                <td>${paciente.fnac}</td>
-                                <td>${paciente.especie}</td>
-                                <td>${paciente.raza}</td>
-                                <td>${paciente.sexo}</td>
-                                <td>${paciente.peso}</td>
-                                <td>${paciente.diagnostico}</td>
-                                </tr>
-                                
-            })
-            contenedor.innerHTML = resultados
+const baseURL   = "https://hwwyuypdcxdigyfznqcw.supabase.co";
+const apiCall   = '/rest/v1/f-registro'
+const apiKey    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjQzODM4OTEyLCJleHAiOjE5NTk0MTQ5MTJ9.tNykPvbzGeUsS7UoP2mNJNBjKUG6R76HPcNtLT1TjBc'
+const url       = baseURL + apiCall
 
-            
-        }
-        fetch(url, {            
-            headers: {
-                "Content-Type": "application/json",
-                "apikey": apiKey,
-                "authorization": "Bearer "+apiKey
-            },
-        })
-        .then(Response => Response.json() )
-        .then( data => mostrar(data) )
-        .catch( error => console.log(resultados) )
 
-        
-        
-        
+const mostrar = (pacientes) => {
+    pacientes.array.forEach(paciente => {
+        resultados +=   ` 
+                        <tr>
+                        <td>${paciente.id}</td>
+                        <td>${paciente.pname}</td>
+                        <td>${paciente.fnac}</td>
+                        <td>${paciente.especie}</td>
+                        <td>${paciente.raza}</td>
+                        <td>${paciente.sexo}</td>
+                        <td>${paciente.peso}</td>
+                        <td>${paciente.diagnostico}</td>
+                        </tr>
+                    ` 
+                        
+    })
+    contenedor.innerHTML = resultados
+}
+fetch(url, {  
+              
+    headers: {
+        "Content-Type": "application/json",
+        "apikey": apiKey,
+        "authorization": "Bearer "+apiKey
+    },
+    
+})
+.then(Response => Response.json() )
+    .then( data => mostrar(data) )
+    .catch( error => console.log(resultados) )
+    .catch(console.log(resultados))
+
