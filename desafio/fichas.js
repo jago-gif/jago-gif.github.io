cb09f3d5eb39ca9de7c5e32742211e43ffffa5c7
jago-gif <halcon1411@gmail.com>
2022-02-04 18:19:43 -0300

    intento 11239482 para que funcione el mostrar tabla

--- desafio/fichas.js
+++ desafio/fichas.js
@@ -1,47 +1,47 @@
+const { createClient } = supabase
+const baseURL   = "https://hwwyuypdcxdigyfznqcw.supabase.co";
+const apiCall   = '/rest/v1/registro'
+const apiKey    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjQzODM4OTEyLCJleHAiOjE5NTk0MTQ5MTJ9.tNykPvbzGeUsS7UoP2mNJNBjKUG6R76HPcNtLT1TjBc'
+const url       = baseURL + apiCall
+
 //variable contenedor
 const contenedor = document.querySelector("tbody")
 let resultados = ''
 
-
-const baseURL   = "https://hwwyuypdcxdigyfznqcw.supabase.co";
-const apiCall   = '/rest/v1/f-registro'
-const apiKey    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjQzODM4OTEyLCJleHAiOjE5NTk0MTQ5MTJ9.tNykPvbzGeUsS7UoP2mNJNBjKUG6R76HPcNtLT1TjBc'
-const url       = baseURL + apiCall
-
-
-const mostrar = (pacientes) => {
-    pacientes.array.forEach(paciente => {
+const mostrar = (registros) => {
+    registros.array.forEach(registro => {
         resultados +=   ` 
-                        <tr>
-                        <td>${paciente.id}</td>
-                        <td>${paciente.pname}</td>
-                        <td>${paciente.fnac}</td>
-                        <td>${paciente.especie}</td>
-                        <td>${paciente.raza}</td>
-                        <td>${paciente.sexo}</td>
-                        <td>${paciente.peso}</td>
-                        <td>${paciente.diagnostico}</td>
-                        </tr>
-                    ` 
+                         <tr>
+                             <td>${registro.id}</td>
+                             <td>${registro.pname}</td>
+                             <td>${registro.fnac}</td>
+                             <td>${registro.especie}</td>
+                             <td>${registro.raza}</td>
+                             <td>${registro.sexo}</td>
+                             <td>${registro.peso}</td>
+                             <td>${registro.diagnostico}</td>
+                             
+                         </tr>
+                        ` 
                         
-    });
-    contenedor.innerHTML = resultados
+    })
+    contenedor.innerHTML=resultados
 }
-fetch(url, {  
-              
+
+    fetch(url, {              
     headers: {
         method: 'GET',
-
         "Content-Type": "application/json",
         "apikey": apiKey,
         "authorization": "Bearer "+apiKey
-    },
-    
+        },
 })
 .then(Response => Response.json() )
-    .then( data => mostrar(data) )
-    .catch( error => console.log(resultados) )
-    .catch(console.log(resultados))
+.then( data => mostrar(data) )
+.catch(error => console.log(error) )
+
+
+ 
+
 
     
-
