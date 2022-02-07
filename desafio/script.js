588747f960e1340f21a62860c86e5ceb9b8f5033
jago-gif <halcon1411@gmail.com>
2022-02-07 05:14:37 -0300

    cambio a formulario de horas de atencion

--- desafio/script.js
+++ desafio/script.js
@@ -1,7 +1,7 @@
 
 // formatear tipo de fecha//
 var dateControl = document.querySelector('input[type="date"]');
-dateControl.value = '2017-06-01';
+dateControl.value = "2022-02-08";
 
 function registrarPaciente(event){    
     // evito que el formulario haga un submit normal
@@ -14,28 +14,27 @@ function registrarPaciente(event){
     
     const pname = document.getElementById("pname").value
     const tutor = document.getElementById("tutor").value
-    const fnac = document.getElementById("fnac").value
+    const fecha = document.getElementById("fecha").value
     const especie = document.getElementById("especie").value
     const raza = document.getElementById("raza").value
-    const sexo = document.getElementById("sexo").value
-    const peso = document.getElementById("peso").value
-    const diagnostico = document.getElementById("diagnostico").value
+    const atencion = document.getElementById("atencion").value
+    const hora = document.getElementById("hora").value
     
 
         //envio de datos por API REST
         const baseURL   = "https://hwwyuypdcxdigyfznqcw.supabase.co";
-        const apiCall   = '/rest/v1/registro'
+        const apiCall   = '/rest/v1/horas'
         const apiKey    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjQzODM4OTEyLCJleHAiOjE5NTk0MTQ5MTJ9.tNykPvbzGeUsS7UoP2mNJNBjKUG6R76HPcNtLT1TjBc'
         const url       = baseURL + apiCall
         const paciente  = {
             pname,
             tutor,
-            fnac,      
+            fecha,      
             especie,         
-            raza,      
-            sexo,      
-            peso,      
-            diagnostico,
+            raza,            
+            atencion, 
+            hora     
+            
         } 
     
         fetch(url, {
