3f60a99a431265d16609ed8a6de45e769587cfe5
jago-gif <halcon1411@gmail.com>
2022-02-11 18:38:21 -0300

    carta de productos con api supabase

--- js/carta.js
+++ js/carta.js
@@ -2,11 +2,18 @@ const baseURL   = "https://yznmxccjhpfqrqrttdqf.supabase.co";
 const apiCallMsyc   = '/rest/v1/menusyc'
 const apiCallEmpa   = '/rest/v1/menuEmpa'
 const apiCallPlatos   = '/rest/v1/Platos'
+const apiCallBbstbs   = '/rest/v1/Bebestibles'
+const apiCallPostres   = '/rest/v1/Postres'
+
 
 const apiKey    = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6bm14Y2NqaHBmcXJxcnR0ZHFmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NDU5NjkyOSwiZXhwIjoxOTYwMTcyOTI5fQ.uGw73zmCWanln_lWnXiQI18o3H_ErFAe3xChid3JNno'
 const urlMsyc       = baseURL + apiCallMsyc
 const urlEmpa       = baseURL + apiCallEmpa
 const urlPlatos       = baseURL + apiCallPlatos
+const urlBbstbs       = baseURL + apiCallBbstbs
+const urlPostres       = baseURL + apiCallPostres
+
+
 
 
 
@@ -126,4 +133,79 @@ fetch(urlPlatos, {
     return error;
   });
  
-  
+//mostrar menu Bebestibles
+const contenedorBbstbs = document.querySelector("#Bbstbs");
+let resultadosBbstbs = "";
+
+const mostrarBbstbs = (MenuBbstbs) => {
+    MenuBbstbs.forEach((Bebestibles) => {
+    resultadosBbstbs += ` 
+                         <tr>
+                             <td>${Bebestibles.Nombre}</td>
+                             <td>${Bebestibles.contenido}</td>
+                             <td>${Bebestibles.precio}</td>
+                         </tr>
+                        `;
+  });
+  contenedorBbstbs.innerHTML = resultadosBbstbs;
+};
+
+fetch(urlBbstbs, {
+  headers: {
+    method: "GET",
+    "Content-Type": "application/json",
+    apikey: apiKey,
+    authorization: "Bearer " + apiKey,
+  },
+})
+  .then((response) => {
+    console.dir(response);
+    return response.json();
+  })
+  .then((data) => {
+    console.dir(data);
+    mostrarBbstbs(data);
+    return data;
+  })
+  .catch((error) => {
+    console.dir(error);
+    return error;
+  });  
+  //mostrar menu Postres
+const contenedorPostres = document.querySelector("#Postres");
+let resultadosPostres = "";
+
+const mostrarPostres = (MenuPostres) => {
+    MenuPostres.forEach((Bebestibles) => {
+    resultadosPostres += ` 
+                         <tr>
+                             <td>${Bebestibles.Nombre}</td>
+                             <td>${Bebestibles.contenido}</td>
+                             <td>${Bebestibles.precio}</td>
+                         </tr>
+                        `;
+  });
+  contenedorPostres.innerHTML = resultadosPostres;
+};
+
+fetch(urlPostres, {
+  headers: {
+    method: "GET",
+    "Content-Type": "application/json",
+    apikey: apiKey,
+    authorization: "Bearer " + apiKey,
+  },
+})
+  .then((response) => {
+    console.dir(response);
+    return response.json();
+  })
+  .then((data) => {
+    console.dir(data);
+    mostrarPostres(data);
+    return data;
+  })
+  .catch((error) => {
+    console.dir(error);
+    return error;
+  });  
